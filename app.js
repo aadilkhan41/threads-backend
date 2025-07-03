725441fa051a3912df31db309c616da5e5557a09
Aadil <aadilkhan3007@gmail.com>
2025-07-04 00:35:36 +0530

    Cors

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ cloudinary.config({
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: true, limit: "50mb"}));
 app.use(cookieParser());
-app.use(cors());
 app.use(express.static("dist"));
 
 app.use("/api/users", userRouter);
